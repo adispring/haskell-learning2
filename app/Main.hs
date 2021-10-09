2f9d137274a0463b6462bc82b4238e7f3931c519
adispring <sunnyadi@163.com>
2021-10-10 06:28:43 +0800

    feat: module 不写 () 会默认导出所有的函数

--- app/Main.hs
+++ app/Main.hs
@@ -4,16 +4,12 @@ module Main where
 import ListII ()
 import Data.List ( nub )
 import Chapter2.Data ( Position(MakePosition) )
-import Chapter2.PatternMatching (distance)
-
-addOneMaybe :: Maybe Int -> Maybe Int
-addOneMaybe (Just x) = Just (x + 1)
-addOneMaybe Nothing =  Nothing
+import Chapter2.PatternMatching (distanceLetIn)
 
 main :: IO ()
 main = print d
        where
          pointA = MakePosition 0 0
          pointB = MakePosition 3 4
-         d = distance pointA pointB
+         d = distanceLetIn pointA pointB
 
--- src/Chapter2/PatternMatching.hs
+++ src/Chapter2/PatternMatching.hs
@@ -1,4 +1,4 @@
-module Chapter2.PatternMatching (distance) where
+module Chapter2.PatternMatching where
 import Chapter2.Data ( Position(..) )
 
 distance :: Position -> Position -> Double
@@ -7,4 +7,14 @@ distance p1 p2 =
     MakePosition x1 y1 -> 
       case p2 of
         MakePosition x2 y2 -> sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+
+distanceV2 :: Position -> Position -> Double
+distanceV2 (MakePosition x1 y1) (MakePosition x2 y2) =
+  sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
+
+distanceLetIn :: Position -> Position -> Double
+distanceLetIn p1 p2 =
+  let MakePosition x1 y1 = p1
+      MakePosition x2 y2 = p2
+  in sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
 
