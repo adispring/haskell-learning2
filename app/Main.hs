233c3e4f7038ab4df4dd6368389f2788d92fa160
adispring <sunnyadi@163.com>
2021-10-09 22:48:06 +0800

    feat: import Data.List

--- app/Main.hs
+++ app/Main.hs
@@ -2,10 +2,11 @@ module Main where
 
 import Lib ( Position(MakePosition, getX), someFunc )
 import ListII ()
+import Data.List ( nub )
 
 addOneMaybe :: Maybe Int -> Maybe Int
 addOneMaybe (Just x) = Just (x + 1)
 addOneMaybe Nothing =  Nothing
 
 main :: IO ()
-main = print (addOneMaybe (Just 1))
+main = print (nub [1,2,3,3])
