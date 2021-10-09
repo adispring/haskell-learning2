28165e4c24697271c613ddf305b2cdacae896ad8
adispring <sunnyadi@163.com>
2021-10-10 05:58:32 +0800

    feat: 根据提示 export Position(MakePosition)

--- app/Main.hs
+++ app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 -- import Lib ( Position(MakePosition, getX), someFunc )
 import ListII ()
 import Data.List ( nub )
-import Chapter2.Data
+import Chapter2.Data ( Position(MakePosition) )
 
 addOneMaybe :: Maybe Int -> Maybe Int
 addOneMaybe (Just x) = Just (x + 1)
